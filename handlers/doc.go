e64b7056a9f098f9f7c972cf6b7ca9250064fdcc
Martin Angers <martin.n.angers@gmail.com>
2013-04-25 22:16:11 -0400

    clear favicon todo

--- handlers/doc.go
+++ handlers/doc.go
@@ -12,14 +12,19 @@
 //
 // This package adds the following list of handlers:
 //
+// - BasicAuthHandler(http.Handler, func(string, string) (interface{}, bool), string)
+// 		 a Basic Authentication handler.
 // - ContextHandler(http.Handler, int) : a volatile storage map valid only
 //     for the duration of the request, with no locking required.
+// - FaviconHandler(http.Handler, string, time.Duration) : an efficient favicon
+//     handler.
 // - GZIPHandler(http.Handler) : compress the content of the body if the client
 //     accepts gzip compression.
 // - LogHandler(http.Handler, *LogOptions) : customizable request logger.
 // - PanicHandler(http.Handler) : handle panics gracefully so that the client
 //     receives a response (status code 500).
+// - SessionHandler(http.Handler, *SessionOptions) : a cookie-based, store-agnostic
+//     persistent session handler.
 // - StaticFileHandler(string) : serve the contents of a specific file.
 //
-// TODO : FaviconHandler.
 package handlers
